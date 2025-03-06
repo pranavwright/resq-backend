5ee16234afda59ebef772f47665f15c02d14de4e
pranavpspranav2005 <pranavpspranav2005@gmail.com>
2025-03-06 06:50:42 +0000

    mongoserver setup

--- configs/index.js
+++ configs/index.js
@@ -2,9 +2,11 @@ import 'dotenv/config'
 
 const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD || 'wright'
 const MONGODB_USER = process.env.MONGODB_USER || 'resq'
+const MONGODB_IP = process.env.MONGODB_IP || '127.0.0.1'
+
 if(process.env.MONGODB_PASSWORD && process.env.MONGODB_USER){
     console.log("DATABASE CAN READ AND WRITE");
 }else{
     console.log("READ ONLY DATABASE")
 }
-export const MONGODB_URL = `mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@34.69.252.224:27017/`
+export const MONGODB_URL = `mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_IP}:27017/`

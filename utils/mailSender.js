7b8af11b70c76a3b6121ecbb71bf16b790bfec43
pranav ps <pranavnecttos@gmail.com>
2025-03-26 17:47:35 +0530

    bug fix

--- utils/mailSender.js
+++ utils/mailSender.js
@@ -3,8 +3,11 @@ import fs from "fs";
 import handlebars from "handlebars";
 import path from "path";
 import { fileURLToPath } from "url";
+import { dirname } from "path";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
